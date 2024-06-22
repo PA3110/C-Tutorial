e6fe7d0c3964a79150749364ea288be604f21816
PA <158666210+PA3110@users.noreply.github.com>
2024-06-22 20:26:42 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,115 @@
+//  #include<stdio.h>
+//  int main() {
+//     // printf("Hello World\n");
+//     // int number;
+//     // int age;
+//     // int price;
+
+//     // int agee = 22;
+//     // float pi = 3.14;
+//     // char percentage = '%';
+    
+//     return 0;
+       
+//  }
+
+// Variables - name of a memory location which stores some data.
+// Rules - case sesnitive(varoables), no comma/blank space, no other symbol other than '_', 1st char is alphabet or '_'
+
+// DataTypes
+
+// Constants-Values that don't change (fixed), Types-Integer Constant(1,2,-1,-2), Real(1.0,2.0,3.14,-24), Character('#','a','A','$')
+
+// Keywords-Reserved words that have special meaning to the compiler.(32 Keywords)
+
+// Program Structure-
+/*
+    #include<stdio.h>
+    int main() {
+        printf("Hello World");
+        return 0;
+    }
+*/
+
+// Comments - Lines that are not part of program - Single Line(//), Multiple Line(/*.....*/)
+
+// Output- Cases--integer(printf("age is %d",age)), real numnbers--(printf("value of pi %f",pi)), characters(printf("star looks like this %c",star))
+
+// #include<stdio.h>
+// int main() {
+//     int age = 22;
+//     float pi = 3.14;
+//     char percentage = '%';
+
+//     printf("age is %d. \n", age);
+//     printf("age is %f. \n", pi);
+//     printf("age is %c. \n", percentage);
+//     return 0;
+// }
+
+//Input---scaf("%d", &age);
+
+// #include<stdio.h>
+// int main() {
+//     // int a, b;
+//     // printf("enter a: \n");
+//     // scanf("%d", &a);
+//     // printf("enter b: \n");
+//     // scanf("%d", &b);
+//     // printf("sum of a & b is : %d \n", a+b);
+//     int age;
+//     printf("enter age: ");
+//     scanf("%d", &age);
+//     printf("age is: %d", age);
+//     return 0;
+// }
+
+// Compilation-A computer program that translates C code into machine code.....Hello.c-->C Compiler-->a.exe(Windows),a.out(linux & mac)
+
+// Practice Qs 1 (Area of Square)
+
+// #include<stdio.h>
+// //area of square
+// int main() {
+//     int side;
+//     scanf("%d", &side);
+//     printf("%d", side * side);
+//     return 0;
+// }
+
+// Practice Qs 2 (Area of Circle)
+
+// #include<stdio.h>
+// //area of square
+// int main() {
+//     float radius;   
+//     scanf("%f", &radius);
+//     printf("%f", 3.14 * radius * radius);
+//     return 0;
+// }
+
+// Homework Set
+
+// Ques1-WAP to calcultae perimeter of rectangle. Take sides, a&b, from the user.
+
+// #include<stdio.h>
+// int main() {
+//     int a,b;
+//     printf("enter a: \n");
+//     scanf("%d", &a);
+//     printf("enter b: \n");
+//     scanf("%d", &b);
+//     printf("Perimeter of Rectangle : %d \n", 2*(a+b));
+// }
+
+// Ques2-Take a number from user and output its cube(n*n*n).
+
+// #include<stdio.h>
+// int main() {
+//     int n;
+//     printf("enter n: \n");
+//     scanf("%d", &n);
+//     printf("Cube of n: %d \n", n*n*n);
+// }
+
+// Ques3-Write comments for programs a&b.
